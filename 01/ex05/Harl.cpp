13e2c6368306996c3cf05ef545ec5ba1668f8184
suiramdev <mariusnouchet@gmail.com>
2023-06-25 16:01:54 +0200

    :sparkles: Done with 01/ex05

---
+++ 01/ex05/Harl.cpp
@@ -0,0 +1,39 @@
+#include "Harl.hpp"
+#include <iostream>
+
+void Harl::debug()
+{
+    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+}
+
+void Harl::info()
+{
+    std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+}
+
+void Harl::warning()
+{
+    std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
+}
+
+void Harl::error()
+{
+    std::cout << "This is unacceptable, I want to speak to the manager now." << std::endl;
+}
+
+void Harl::complain(std::string level)
+{
+    // Array of function pointers
+    void (Harl::*complaints[])() = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+    std::string levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+
+    for (int i = 0; i < 4; i++)
+    {
+        if (levels[i] == level)
+        {
+            (this->*complaints[i])();
+            return ;
+        }
+    }
+    std::cout << "Invalid level" << std::endl;
+}
---
+++ 01/ex05/Harl.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <string>
+
+class Harl
+{
+private:
+    void debug();
+    void info();
+    void warning();
+    void error();
+public:
+    void complain(std::string level);
+};
