05ec27ef9fb973a9e947b80b0b652853e889ad00
suiramdev <mariusnouchet@gmail.com>
2023-07-18 12:57:14 +0200

    :art: Improved tests from 03/ex00

--- 03/ex00/main.cpp
+++ 03/ex00/main.cpp
@@ -3,20 +3,15 @@
 
 int main()
 {
-    ClapTrap clapTrap1("ClapTrap1");
-    ClapTrap clapTrap2("ClapTrap2");
+    ClapTrap a("ClapTrap A");
+    ClapTrap b("ClapTrap B");
 
-    clapTrap1.attack("ClapTrap2");
-    clapTrap2.beRepaired(5);
-    clapTrap2.attack("ClapTrap1");
-    clapTrap2.attack("ClapTrap1");
-    clapTrap1.beRepaired(10);
-    std::cout << "-- ClapTrap1 --" << std::endl;
-    std::cout << "Hit points : " << clapTrap1.getHitPoints() << std::endl;
-    std::cout << "Energy points : " << clapTrap1.getEnergyPoints() << std::endl;
-    std::cout << "Attack damage : " << clapTrap1.getAttackDamage() << std::endl;
-    std::cout << "-- ClapTrap2 --" << std::endl;
-    std::cout << "Hit points : " << clapTrap2.getHitPoints() << std::endl;
-    std::cout << "Energy points : " << clapTrap2.getEnergyPoints() << std::endl;
-    std::cout << "Attack damage : " << clapTrap2.getAttackDamage() << std::endl;
+    a.attack("ClapTrap B");
+    b.takeDamage(a.getAttackDamage());
+    b.attack("ClapTrap A");
+    a.takeDamage(b.getAttackDamage());
+    a.beRepaired(10);
+    b.beRepaired(10);
+    std::cout << "A has " << a.getHitPoints() << " hit points and " << a.getEnergyPoints() << " energy points" << std::endl;
+    std::cout << "B has " << b.getHitPoints() << " hit points and " << b.getEnergyPoints() << " energy points" << std::endl;
 }
