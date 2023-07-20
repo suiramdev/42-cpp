8ee428cee15757d073771aa59d549a2a4dfae275
suiramdev <mariusnouchet@gmail.com>
2023-07-20 12:49:41 +0200

    :art: Removed unuseful tabbing

--- 01/ex03/HumanA.hpp
+++ 01/ex03/HumanA.hpp
@@ -6,8 +6,8 @@
 class HumanA
 {
 private:
-    Weapon&		_weapon;
-    std::string	_name;
+    Weapon& _weapon;
+    std::string _name;
 
 public:
     HumanA(std::string name, Weapon& weapon);
--- 01/ex03/HumanB.hpp
+++ 01/ex03/HumanB.hpp
@@ -6,8 +6,8 @@
 class HumanB
 {
 private:
-    Weapon		*_weapon;
-    std::string	_name;
+    Weapon *_weapon;
+    std::string _name;
 
 public:
     HumanB(std::string name);
