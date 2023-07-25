ae5ac72da7bfe2ebd0d62384611c23a5788fe6d3
suiramdev <mariusnouchet@gmail.com>
2023-07-25 15:29:09 +0200

    :test_tube: Improved failing tests on 05/ex00

--- 05/ex00/main.cpp
+++ 05/ex00/main.cpp
@@ -3,12 +3,19 @@
 
 int main()
 {
-    Bureaucrat b("b", 1);
-    std::cout << b << std::endl;
-    b.decGrade();
-    std::cout << b << std::endl;
-    b.incGrade();
-    std::cout << b << std::endl;
-    b.incGrade();
-    std::cout << b << std::endl;
+    try {
+        std::cout << "-- Bureaucrat with grade of 1, increased --" << std::endl;
+        Bureaucrat b("b", 1);
+        std::cout << b << std::endl;
+        b.incGrade();
+        std::cout << b << std::endl;
+    } catch (std::exception &e) {
+        std::cout << e.what() << std::endl;
+    }
+    try {
+        std::cout << "-- Bureaucrat with grade of 151 --" << std::endl;
+        Bureaucrat a("a", 151);
+    } catch (std::exception &e) {
+        std::cout << e.what() << std::endl;
+    }
 }
