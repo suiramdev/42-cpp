527b4ffe535fd6ae70b6dfca357ed748e72be60b
suiramdev <mariusnouchet@gmail.com>
2023-07-27 11:50:50 +0200

    :sparkles: done with 05/ex01

---
+++ 05/ex01/Form.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <exception>
+#include <string>
+
+class Form
+{
+private:
+    const std::string _name;
+    bool _signed;
+    const int _signGrade;
+    const int _execGrade;
+public:
+    Form();
+    Form(std::string name);
+    Form(std::string name, int signGrade, int execGrade);
+    Form(const Form &form);
+    Form &operator=(const Form &form);
+    virtual ~Form();
+
+    friend std::ostream& operator<<(std::ostream &stream, const Form& form);
+
+    const std::string getName() const;
+    bool isSigned() const;
+    void beSigned(class Bureaucrat &bureaucrat);
+    int getSignGrade() const;
+    int getExecGrade() const;
+
+    class GradeTooHighException : public std::exception
+    {
+        public:
+            const char* what() const throw();
+    };
+
+    class GradeTooLowException : public std::exception
+    {
+        public:
+            const char* what() const throw();
+    };
+};
