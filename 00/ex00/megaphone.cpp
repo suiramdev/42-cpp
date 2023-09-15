ff810014e36fc274f17fbb9a2f58c44c11776b35
suiramdev <suiramdev.official@gmail.com>
2023-09-15 14:40:48 +0200

    :sparkles: Added a fallback log when using megaphone w/ no args

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -1,7 +1,15 @@
+#include <cstdlib>
 #include <iostream>
 
 int main(int argc, char *argv[])
 {
+    if (argc == 1)
+    {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+        return (EXIT_FAILURE);
+    }
     for (int i = 1; i < argc; i++)
         std::cout << argv[i];
+    std::cout << std::endl;
+    return (EXIT_SUCCESS);
 }
