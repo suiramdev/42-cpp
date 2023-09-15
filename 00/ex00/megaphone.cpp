7f1a95fe417422d6589bd9b66ba2ec5c8644da31
suiramdev <mariusnouchet@gmail.com>
2023-09-15 16:33:29 +0200

    :sparkles: Set the megaphole to print out in caps lock

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -9,7 +9,8 @@ int main(int argc, char *argv[])
         return (EXIT_FAILURE);
     }
     for (int i = 1; i < argc; i++)
-        std::cout << argv[i];
+        for (size_t j = 0; j < strlen(argv[i]); j++)
+            std::cout << (char)toupper(argv[i][j]);
     std::cout << std::endl;
     return (EXIT_SUCCESS);
 }
