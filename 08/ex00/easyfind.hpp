feb5b03e783a55ae038ba58eaf166c196d211cc9
suiramdev <me@suiram.dev>
2024-02-21 14:33:52 +0100

    :art: Use of for loop instead of while

--- 08/ex00/easyfind.hpp
+++ 08/ex00/easyfind.hpp
@@ -2,12 +2,10 @@
 #include <exception>
 
 template <typename T> typename T::iterator easyfind(T &container, int value) {
-  typename T::iterator it = container.begin();
-  typename T::iterator ite = container.end();
-  while (it != ite) {
+  for (typename T::iterator it = container.begin(); it != container.end();
+       ++it) {
     if (*it == value)
       return it;
-    ++it;
   }
   throw std::exception();
 }
