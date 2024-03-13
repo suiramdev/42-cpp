12e0990554a766bf13fdc22775843bbc3ab7651b
suiramdev <suiramdev.official@gmail.com>
2024-03-13 15:42:28 +0100

    :constrution: WIP ex02

--- 09/ex02/PmergeMe.hpp
+++ 09/ex02/PmergeMe.hpp
@@ -5,6 +5,7 @@
 #include <memory>
 #include <utility>
 #include <iostream>
+#include <stdint.h>
 
 template <template <typename, typename> class ContainerType> class PmergeMe {
 private:
@@ -28,8 +29,6 @@ private:
         } else {
           pairs.push_back(std::make_pair(container[i + 1], container[i]));
         }
-      } else {
-        pairs.push_back(std::make_pair(container[i], -1)); // NOTE: That's a weird trick lol
       }
     }
 
@@ -83,7 +82,7 @@ private:
     }
   }
 
-  static typename ChainContainer::const_iterator findInsertPosition(const ChainContainer &container, int value) {
+  static typename ChainContainer::iterator findInsertPosition(ChainContainer &container, int value) {
     size_t low = 0;
     size_t high = container.size();
 
@@ -96,11 +95,35 @@ private:
             high = mid;
     }
 
-    return container.begin() + low;
+    typename ChainContainer::iterator it = container.begin();
+    std::advance(it, low);
+
+    return it;
   }
 
-  static int jacobsthal(int n) {
-    return static_cast<int>((std::pow(2, n) + std::pow(-1, n - 1)) / 3);
+  static ChainContainer generateInsertionIndexes(size_t size) {
+    ChainContainer indexes;
+    int jacobsthalNumbers[size + 1];
+    int lastJacobsthalNumber = 2;
+
+    jacobsthalNumbers[0] = 0;
+    jacobsthalNumbers[1] = 1;
+    for (size_t i = 2; i < size; i++) {
+      
+      jacobsthalNumbers[i] = jacobsthalNumbers[i - 1] + 2 * jacobsthalNumbers[i - 2];
+
+      if (i != 2) {
+        indexes.push_back(jacobsthalNumbers[i]);
+      }
+
+      for (int j = lastJacobsthalNumber; j < jacobsthalNumbers[i]; j++) {
+        indexes.push_back(j);
+      }
+
+      lastJacobsthalNumber = jacobsthalNumbers[i];
+    }
+
+    return indexes;
   }
 
 public:
@@ -124,50 +147,41 @@ public:
   static double sort(ChainContainer &container) {
     clock_t start = clock();
 
+    int last = -1;
+
+    if (container.size() % 2 != 0) {
+      last = container[container.size() - 1];
+    }
+
     // We form pairs, sort the elements of each pair and sort the entire
     // container (based on the first value of the pair, otherwise known as `a`).
     PairContainer sortedPairs = makeSortedPairs(container);
 
     // Finally, we form a list from the `a`s, the "main-chain", into which we will proceed insertion from the `b`s.
-    ChainContainer mainChain;
+    container.clear();
+    container.push_back(sortedPairs[0].second);
     for (typename PairContainer::const_iterator it = sortedPairs.begin(); it != sortedPairs.end(); ++it) {
-      if (it == sortedPairs.begin()) {
-        mainChain.push_back(it->second);
-      }
-      mainChain.push_back(it->first);
+      container.push_back(it->first);
     }
 
-    static const std::uint_least64_t jacobsthal_diff[] = {
-        2u, 2u, 6u, 10u, 22u, 42u, 86u, 170u, 342u, 682u, 1366u,
-        2730u, 5462u, 10922u, 21846u, 43690u, 87382u, 174762u, 349526u, 699050u,
-        1398102u, 2796202u, 5592406u, 11184810u, 22369622u, 44739242u, 89478486u,
-        178956970u, 357913942u, 715827882u, 1431655766u, 2863311530u, 5726623062u,
-        11453246122u, 22906492246u, 45812984490u, 91625968982u, 183251937962u,
-        366503875926u, 733007751850u, 1466015503702u, 2932031007402u, 5864062014806u,
-        11728124029610u, 23456248059222u, 46912496118442u, 93824992236886u, 187649984473770u,
-        375299968947542u, 750599937895082u, 1501199875790165u, 3002399751580331u,
-        6004799503160661u, 12009599006321322u, 24019198012642644u, 48038396025285288u,
-        96076792050570576u, 192153584101141152u, 384307168202282304u, 768614336404564608u,
-        1537228672809129216u, 3074457345618258432u, 6148914691236516864u
-    };
+    ChainContainer insertionIndexes = generateInsertionIndexes(sortedPairs.size());
 
-    for (size_t i = 0; ; i++) {
-      std::uint_least64_t dist = jacobsthal_diff[i];
-      if (dist >= sortedPairs.size()) break;
+    for (size_t i = 0; i < insertionIndexes.size(); i++) {
+      if (size_t(insertionIndexes[i]) >= sortedPairs.size()) {
+        continue;
+      }
 
       typename PairContainer::const_iterator it = sortedPairs.begin();
-      std::advance(it, dist);
+      std::advance(it, insertionIndexes[i]);
 
-      if (it->second == -1) {
-        continue; // NOTE: 🤡
-      }
-
-      typename ChainContainer::const_iterator insertionPoint = findInsertPosition(mainChain, it->second);
-      mainChain.insert(insertionPoint, it->second);
+      typename ChainContainer::iterator insertionPoint = findInsertPosition(container, it->second);
+      container.insert(insertionPoint, it->second);
     }
 
-    // We replace the container passed in parameter with our sorted list.
-    container = mainChain;
+    if (last != -1) {
+      typename ChainContainer::iterator insertionPoint = findInsertPosition(container, last);
+      container.insert(insertionPoint, last);
+    }
 
     return static_cast<double>(clock() - start) / CLOCKS_PER_SEC;
   }
