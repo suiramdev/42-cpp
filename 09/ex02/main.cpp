763a92c2e19e2f0462ff45941af3dea0c0533d57
suiramdev <me@suiram.dev>
2024-03-19 11:58:49 +0100

    :sparkles: Split numbers in string argv

--- 09/ex02/main.cpp
+++ 09/ex02/main.cpp
@@ -3,6 +3,19 @@
 #include <iostream>
 #include <vector>
 #include <deque>
+#include <sstream>
+
+std::string concatenate(char *array[], size_t size) {
+  std::string result;
+  for (size_t i = 0; i < size; i++) {
+    if (i > 0) {
+      result += " ";
+    }
+    result += array[i];
+  }
+
+  return result;
+}
 
 template<template <typename T, typename = std::allocator<T> > class Container>
 void printContainer(const Container<int> &container) {
@@ -20,19 +33,27 @@ int main(int argc, char *argv[]) {
     return EXIT_FAILURE;
   }
 
-  try {
-    std::vector<int> vec;
-    std::deque<int> deq;
-    for (int i = 1; i < argc; i++) {
-      int number = std::atoi(argv[i]);
-      if (number < 0) {
-        std::cerr << "The sequence contain(s) negative number(s)" << std::endl;
-        return EXIT_FAILURE;
-      }
+  std::string sequence = concatenate(argv + 1, argc - 1);
+
+  std::vector<int> vec;
+  std::deque<int> deq;
+  std::istringstream iss(sequence);
+  while (iss) {
+    int number;
+    iss >> number;
+
+    if (number < 0) {
+      std::cerr << "The sequence contain(s) negative number(s)" << std::endl;
+      return EXIT_FAILURE;
+    }
+
+    if (iss) {
       vec.push_back(number);
       deq.push_back(number);
     }
+  }
 
+  try {
     clock_t start = clock();
     std::vector<int> sortedVec = PmergeMe<std::vector>::sort(vec);
     double sortVectorTime = static_cast<double>(clock() - start) / CLOCKS_PER_SEC;
